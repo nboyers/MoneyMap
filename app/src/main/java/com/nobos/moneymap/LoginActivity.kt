f3bc5ccb6c96dc1bd737949330896c544a35f533
nboyers <frenttv@gmail.com>
2023-03-31 20:22:15 -0400

    Finished up Login UI and logic

---
+++ app/src/main/java/com/nobos/moneymap/LoginActivity.kt
@@ -0,0 +1,48 @@
+package com.nobos.moneymap
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        val emailEditText: EditText = findViewById(R.id.emailEditText)
+        val passwordEditText: EditText = findViewById(R.id.passwordEditText)
+        val loginButton: Button = findViewById(R.id.loginButton)
+
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please fill in all fields.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        loginButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
--- app/src/main/java/com/nobos/moneymap/MainActivity.kt
+++ app/src/main/java/com/nobos/moneymap/MainActivity.kt
@@ -1,45 +1,107 @@
 package com.nobos.moneymap
-
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.nobos.moneymap.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        mAuth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
+        // Check if the user is logged in, otherwise navigate to LoginActivity
+        val currentUser = mAuth.currentUser
+
+        if (currentUser != null) {
+            if (currentUser.isEmailVerified) {
+                startActivity(Intent(this, MainActivity::class.java))
+                this.finish()
+            }
         }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return false; //TODO: Make this work
+
+
+
+        // Initialize UI components
+        val totalIncomeTextView: TextView = findViewById(R.id.totalIncomeTextView)
+        val remainingBalanceTextView: TextView = findViewById(R.id.remainingBalanceTextView)
+        val incomeEditText: EditText = findViewById(R.id.incomeEditText)
+        val foodExpenseEditText: EditText = findViewById(R.id.foodExpenseEditText)
+        val gasExpenseEditText: EditText = findViewById(R.id.gasExpenseEditText)
+        val entertainmentExpenseEditText: EditText = findViewById(R.id.entertainmentExpenseEditText)
+        val savingsEditText: EditText = findViewById(R.id.savingsEditText)
+        val saveButton: Button = findViewById(R.id.saveButton)
+        val signOutButton: Button = findViewById(R.id.signOutButton)
+
+        // Set up listeners
+        saveButton.setOnClickListener {
+            val income = incomeEditText.text.toString().toIntOrNull()
+            val foodExpense = foodExpenseEditText.text.toString().toIntOrNull()
+            val gasExpense = gasExpenseEditText.text.toString().toIntOrNull()
+            val entertainmentExpense = entertainmentExpenseEditText.text.toString().toIntOrNull()
+            val savings = savingsEditText.text.toString().toIntOrNull()
+
+            currentUser?.let { user ->
+                val userId = user.uid
+                val budget = Budget(
+                    id = userId,
+                    userId = userId,
+                    income = income ?: 0,
+                    foodExpense = foodExpense ?: 0,
+                    gasExpense = gasExpense ?: 0,
+                    entertainmentExpense = entertainmentExpense ?: 0,
+                    savings = savings ?: 0
+                )
+
+                db.collection("budgets").document(userId).set(budget)
+                    .addOnSuccessListener {
+                        // Handle success
+                    }
+                    .addOnFailureListener { e ->
+                        // Handle failure
+                        e.stackTraceToString()
+                    }
+            }
+        }
+
+        // Fetch data from Firestore and update UI
+        currentUser?.let { user ->
+            val userId = user.uid
+            db.collection("budgets").document(userId).get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        val budget = document.toObject(Budget::class.java)
+
+                        budget?.let { b ->
+                            totalIncomeTextView.text = b.income.toString()
+                            val remainingBalance = b.income - b.foodExpense - b.gasExpense - b.entertainmentExpense - b.savings
+                            remainingBalanceTextView.text = remainingBalance.toString()
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    // Handle failure
+                }
+        }
+
+        // Sign out
+        signOutButton.setOnClickListener {
+            mAuth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
